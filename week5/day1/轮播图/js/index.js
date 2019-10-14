485187ba1b8eb439798e9196d12297d5a6d8534a
yaohuipeng <2544569199@qq.com>
2019-10-14 16:46:18 +0800

    y

--- week5/day1/轮播图/js/index.js
+++ week5/day1/轮播图/js/index.js
@@ -1,44 +1,44 @@
-var banner = (function () {
-    let idSelector = ''
+var banner = (function(){
+    let idSelector = '';
     let $box = null,
         $ul = null,
         $lis = null,
-        $tipBpx = null,
+        $tipBox = null,
         $tips = null,
         $leftBtn = null,
-        $rightBtn = null
-    var n = 0, timer = null
-    function throttle(fn, wait = 500) {
-        let flag = true
-        return function () {
-            if (!flag) return
-            flag = false
+        $rightBtn = null;
+    var n = 0, timer = null; // n 控制了全局图片对应的索引  
+    function throttle(fn,wait=500){
+        let flag = true;
+        return function(){
+            if(!flag)return;
+            flag = false;
             setTimeout(() => {
-                flag
-            })
+                flag = true;
+                fn.apply(this,arguments)
+            }, wait);
         }
-
-
     }
     function initEle() {
-        $box = $(idSelector)
-        $ul = $box.find('.img_box ul')
-        $lis = $box.find('.img_box ul li')
-        $tipBpx = $box.find('.tip_box')
-        $tips = $box.children('.tip')
-        $leftBtn = $box.find('.left_btn')
-        $rightBtn = $box.find('.right_btn')
+        $box = $(idSelector);
+        $ul = $box.find('.img_box ul'),
+            $lis = $box.find('.img_box ul li'),
+            $tipBox = $box.find('.tip_box'),
+            $tips = $tipBox.children('.tip'),
+            $leftBtn = $box.find('.left_btn'),
+            $rightBtn = $box.find('.right_btn');
 
-        $lis.eq(0).show().siblings().hide()
+        $lis.eq(0).show().siblings().hide();
     }
     function getData() {
         $.ajax({
             url: './data.json',
             success: function (data) {
-                render(data)
-                initEle()
-                autoMove()
-                eventBind()
+                //
+                render(data);
+                initEle();
+                autoMove();
+                eventBind();
             },
             error: function () {
                 alert('失败')
@@ -46,63 +46,66 @@ var banner = (function () {
         })
     }
     function render(data) {
-        let str = ''
-        let tipstr = ''
+        let str = '', tipStr = '';
         data.forEach((item, index) => {
             str += `<li>
-                    <img src="${item.img}" alt="">
-                        <p>${item.desc}</p>
-                    </li>`
-            tipstr += (index == 0 ? `<span class="tip current"></span> ` : `<span class="tip"></span> `)
+                <img src="${item.img}" alt="">
+                <p>${item.desc}</p>
+            </li>`;
+            tipStr += (index == 0 ? `<span class="tip current"></span> ` : `<span class="tip"></span> `)
         })
         $ul.html(str)
-        $tipBpx.html(tipstr)
+        $tipBox.html(tipStr);
     }
     function move() {
-        n++
+        n++;
         if (n > $lis.length - 1) {
-            n = 0
+            n = 0;
         }
         $lis.eq(n).show().css({ opacity: 0 }).animate({ opacity: 1 }, 300).siblings().animate({ opacity: 0 }, 300, function () {
-            $lis.eq(n).siblings().hide()
+            $lis.eq(n).siblings().hide();
         })
-        autoFocus()
+        autoFocus();
     }
     function autoMove() {
         timer = setInterval(() => {
-            move()
+            move();
         }, 2000)
     }
     function autoFocus() {
-        $tips.eq(n).addclass('current').siblings().removeClass('current')
+        $tips.eq(n).addClass('current').siblings().removeClass('current');
     }
     function eventBind() {
         $box.on('mouseenter', function () {
-            clearInterval(timer)
+            clearInterval(timer);
         })
         $box.on('mouseleave', function () {
-            autoMove()
+            autoMove();
         })
-        $leftBtn.on('clicl', throttle(function () {
-            n--
+        $leftBtn.on('click', throttle(function () {
+            n--;
             if (n < 0) {
-                n = $lis.length - 1
+                n = $lis.length - 1;
             }
-            move()
+
+            n--;
+            move();
         }))
         $rightBtn.on('click', throttle(function () {
-            move()
+            move();
         }))
         $tips.on('click', function () {
-            let index = $(this).init()
-            n = index
+            let index = $(this).index();
+            n = index;
 
+            n--;
+            move();
         })
     }
 
     return {
-        init(id) {
-            idSelector = '#'+this.attr('id')
+        init() {
+            idSelector = '#' + this.attr('id')
             getData()
             initEle()
         }
@@ -117,8 +120,8 @@ $.extend({
 })
 $.fn.extend({
     aaa() {
-        console.log(333);
+        console.log(999)
     },
-    bannerInit:banner.init
+    bannerInit: banner.init
 })
 $('#box').bannerInit()
