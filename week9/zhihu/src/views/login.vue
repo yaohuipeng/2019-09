a9deef3a27819c190fca81b5bea715b1818026b0
yaohuipeng <2544569199@qq.com>
2019-11-29 13:07:06 +0800

    0

---
+++ week9/zhihu/src/views/login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      登录页
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+  data() {
+    return {
+
+    };
+  },
+  components: {
+
+  }
+};
+</script>
+<style lang="less">
+
+</style>
---
+++ week9/zhihu/vue.config.js
@@ -0,0 +1,14 @@
+/* module.exports = {
+    publicPath: '/',
+    devServer: {
+        proxy: {
+            target: 'https://www.zhihu.com/api/v4/'
+        }
+    }
+} */
+module.exports = {
+    publicPath: '/',
+    devServer: {
+        proxy: 'https://www.zhihu.com/api/v4/'
+    }
+}
